398a773157ee66b2211c8564029eca0b38ea24c4
Luiz Prado <113873051+Tomoharuu@users.noreply.github.com>
2026-02-15 18:35:22 -0300

    Adicionando texto na página de passo a passo

--- oscar_streamlit.py
+++ oscar_streamlit.py
@@ -77,6 +77,7 @@ st.write("""
 | `note` | Algumas informações que podem ter grande relevância para alguns dos conflitos com as colunas `name` e `film` estão descritos nesta coluna. |
 """)
 
+# Parte 2 - Tratando os dados da planilha
 st.subheader("2. Tratando os dados da planilha", divider="gray")
 
 st.write("""Vamos começar tentando padronizar os dados das colunas `year` e `film`. O objetivo é termos o mesmo número de linhas entre as duas bases após removermos os valores vazios e outras alterações.
@@ -103,24 +104,75 @@ st.dataframe(df_limpo.value_counts("Year").reset_index().sort_values(by="Year", 
 st.write("Analisando `df2`")
 st.dataframe(df2_limpo.value_counts("year_film").reset_index().sort_values(by="year_film", ascending=True).head())
 
-st.write("Podemos perceber que em 1927 já há uma inconsistência de valores. Vamos averiguar as categorias disponíveis deste ano para as duas tabelas:")
+st.write("Podemos perceber que em 1927 já há uma inconsistência de valores. Vamos averiguar as categorias disponíveis deste ano para as duas tabelas.")
+st.write("Para isso, há dois métodos diferentes em que podemos checar essas informações:")
 
-st.table(df_limpo[df_limpo["Year"] == "1927/28"].value_counts("Category"))
 
-st.table(df2_limpo[df2_limpo["year_film"] == 1927].value_counts("category"))
+with st.expander("1.) Eliminando aos poucos as inconsistências de categorias (em processo)"):
+    st.table(df_limpo[df_limpo["Year"] == "1927/28"].value_counts("Category"))
 
-st.write("Podemos perceber que em uma das tabelas há a categoria `SPECIAL AWARD`, e ela é a responsável pela diferença. Portanto, vamos remover todas as instâncias dessa categoria para verificar se há algum valor restante.")
+    st.table(df2_limpo[df2_limpo["year_film"] == 1927].value_counts("category"))
 
-# Removendo as categorias com SPECIAL AWARD de df2
-filtro2 = df2_limpo["category"] != "SPECIAL AWARD"
-df2_limpo = df2_limpo[filtro2]
+    st.write("Podemos perceber que em uma das tabelas há a categoria `SPECIAL AWARD`, e ela é a responsável pela diferença. Portanto, vamos remover todas as instâncias dessa categoria para verificar se há algum valor restante.")
 
-st.write(f"Total de linhas e colunas: {df2_limpo.shape}")
-st.dataframe(df2_limpo)
+    # Removendo as categorias com SPECIAL AWARD de df2
+    filtro2 = df2_limpo["category"] != "SPECIAL AWARD"
+    df2_limpo = df2_limpo[filtro2]
 
-st.write("Conferindo novamente quais os valores por ano de `df2`:")
-st.dataframe(df2_limpo.value_counts("year_film").reset_index().sort_values(by="year_film", ascending=True))
-st.dataframe(df_limpo.value_counts("Year").reset_index().sort_values(by="Year", ascending=True))
+    st.write(f"Total de linhas e colunas: {df2_limpo.shape}")
+    st.dataframe(df2_limpo)
+
+    st.write("Conferindo novamente quais os valores por ano de `df2`:")
+    st.dataframe(df2_limpo.value_counts("year_film").reset_index().sort_values(by="year_film", ascending=True))
+    st.dataframe(df_limpo.value_counts("Year").reset_index().sort_values(by="Year", ascending=True))
+
+
+
+
+with st.expander("2.) Checando todas as categorias de uma só vez"):
+    st.write("Neste método, iremos extrair todas as categorias e transformá-las em uma lista pela seguinte maneira.")
+    st.write("Para `df`:")
+    st.code("""
+df_categorias = df.dropna(axis=0, subset=["Name", "Film"])
+lista_df = df_categorias.value_counts("Category").reset_index()
+lista_df = lista_df["Category"].sort_values(ascending=True).to_list()""")
+    st.write("Resultado da lista (em forma de tabela):")
+    df_categorias = df.dropna(axis=0, subset=["Name", "Film"])
+    lista_df = df_categorias.value_counts("Category").reset_index()
+    lista_df = lista_df["Category"].sort_values(ascending=True).to_list()
+    st.dataframe(lista_df)
+    # st.code(lista_df, wrap_lines=True)
+
+    st.write("Para `df2`:")
+    st.code("""
+df2_categorias = df2.dropna(axis=0, subset=["name", "film"])
+lista_df2 = df2_categorias.value_counts("category").reset_index()
+lista_df2 = lista_df2["category"].sort_values(ascending=True).to_list()""")
+    st.write("Resultado da lista (em forma de tabela):")
+    df2_categorias = df2.dropna(axis=0, subset=["name", "film"])
+    lista_df2 = df2_categorias.value_counts("category").reset_index()
+    lista_df2 = lista_df2["category"].sort_values(ascending=True).to_list()
+    st.dataframe(lista_df2)
+    # st.code(lista_df2, wrap_lines=True)
+
+    st.write("Agora iremos verificar quais categorias estão presentes em `df2` que não estão em `df` (já que o primeiro tem um número de linhas maior):")
+    st.code("c = list(set(lista_df2) - set(lista_df))", language="python")
+
+    c = list(set(lista_df2) - set(lista_df))
+    c.sort()
+    st.dataframe(c)
+
+st.html("""<h2>Conclusão da análise prévia dos dados</h2>
+Podemos, a partir disso, coletar algumas informações importantes e já tomar algumas decisões de acordo com algumas colunas:""")
+
+st.write("Poderemos agora partir para a análise de dados feita de fato.")
+st.write("")
+
+# Parte 3 - Analisando os dados e levantando gráficos
+st.subheader("3. Tratando os dados da planilha", divider="gray")
+st.write("Texto aqui...")
+
+
 
 st.html("""<br><br><br><br><br><br>
 <details>r
